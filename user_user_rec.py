bd0b2dd752d46d22c2d813d8e06b2c4dd0493b72
DipakDA <dipak19da@gmail.com>
2019-01-23 15:55:08 +0530

    Added ipython nnotebook

--- user_user_rec.py
+++ user_user_rec.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 
-data1 = pd.read_csv("./../netflix-prize-data/updated_data1.txt", sep = ",")
-#data2 = pd.read_csv("./../netflix-prize-data/combined_data_2.txt", sep = ",")
-#data3 = pd.read_csv("./../netflix-prize-data/combined_data_3.txt", sep = ",")
-#data1.columns = ["ALL"]
-print(data1)
+data1 = pd.read_csv("./../netflix-prize-data/updated_data1.txt", sep = ",", header = None)
+data1.columns = ["UserID", "Ratings", "Date", "MovieID"]
+#print(data1)
 
-#for index, row in data1.iterrows():
-#    print(type(row["ALL"]))
+Mean = data1.groupby(["UserID"], as_index = False, sort = False).mean().rename(columns = {"Ratings": "rating_mean"})[["UserID", "rating_mean"]]
+data1 = pd.merge(data1, Mean, on = "UserID", how = "left", sort = False)
+data1["Adjusted_Ratings"] = data1["Ratings"] - data1["rating_mean"]
+#print(data1)
 
-#print(data2)
-#print(data3)
+distinct_users = np.unique(data1["UserID"]).size
+print(distinct_users)
